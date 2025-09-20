a8373b07d939ba974daba3724d45037e1a8c4af3
Mendes-Matheus <msmmendes211@gmail.com>
2025-09-20 18:00:52 -0300

    feat: adiciona spring security

---
+++ src/main/java/mendes/dev95/med_management_system_backend/infra/security/SecurityFilter.java
@@ -0,0 +1,67 @@
+package mendes.dev95.med_management_system_backend.infra.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import mendes.dev95.med_management_system_backend.usuario.entity.Usuario;
+import mendes.dev95.med_management_system_backend.usuario.repository.UsuarioRepository;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Locale;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UsuarioRepository userRepository;
+    private final MessageSource messageSource;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if(login != null){
+            Usuario usuario = userRepository.findByEmail(login)
+                    .orElseThrow(() -> new ResponseStatusException(
+                            HttpStatus.NOT_FOUND,
+                            getMessage("user.notfound")
+                    ));
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    usuario,
+                    null,
+                    usuario.getAuthorities()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+    /**
+     * Metodo auxiliar para buscar mensagens internacionalizadas
+     */
+    private String getMessage(@lombok.NonNull String code, @lombok.NonNull Object... args) {
+        Locale locale = LocaleContextHolder.getLocale(); // detecta locale do request
+        return messageSource.getMessage(code, args, locale);
+    }
+
+}
