cd4de9def408742ca3d412b94901dafc91475255
Mendes-Matheus <msmmendes211@gmail.com>
2025-09-20 12:57:02 -0300

    initial commit

---
+++ src/main/java/mendes/dev95/med_management_system_backend/MedManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package mendes.dev95.med_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mendes/dev95/med_management_system_backend/MedManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mendes.dev95.med_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
