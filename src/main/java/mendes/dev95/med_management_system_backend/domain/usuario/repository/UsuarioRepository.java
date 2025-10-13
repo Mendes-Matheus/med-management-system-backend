41b96451ad39b95199cca9a76c56760f529c073b
Mendes-Matheus <msmmendes211@gmail.com>
2025-10-12 21:06:34 -0300

    adiciona anotação

--- src/main/java/mendes/dev95/med_management_system_backend/domain/usuario/repository/UsuarioRepository.java
+++ src/main/java/mendes/dev95/med_management_system_backend/domain/usuario/repository/UsuarioRepository.java
@@ -2,10 +2,12 @@ package mendes.dev95.med_management_system_backend.domain.usuario.repository;
 
 import mendes.dev95.med_management_system_backend.domain.usuario.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
     Optional<Usuario> findByEmail(String email);
     Optional<Usuario> findById(UUID id);
