5886d589e95e60dd594b369506bdcae261c6ece9
Mendes-Matheus <msmmendes211@gmail.com>
2025-10-18 20:30:48 -0300

    refactor: adiciona id em ProcedimentoSimpleResponseDTO

--- src/main/java/mendes/dev95/med_management_system_backend/domain/procedimento/dto/ProcedimentoSimpleResponseDTO.java
+++ src/main/java/mendes/dev95/med_management_system_backend/domain/procedimento/dto/ProcedimentoSimpleResponseDTO.java
@@ -3,10 +3,12 @@ package mendes.dev95.med_management_system_backend.domain.procedimento.dto;
 import mendes.dev95.med_management_system_backend.domain.estabelecimento.dto.EstabelecimentoSimpleResponseDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public record ProcedimentoSimpleResponseDTO(
-    String nomeProcedimento,
-    String orientacoes,
-    String observacoes,
-    List<EstabelecimentoSimpleResponseDTO> estabelecimentos
+        UUID id,
+        String nomeProcedimento,
+        String orientacoes,
+        String observacoes,
+        List<EstabelecimentoSimpleResponseDTO> estabelecimentos
 ) {}
