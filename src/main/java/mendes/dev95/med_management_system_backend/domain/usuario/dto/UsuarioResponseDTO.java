24bdd93808fe29d681f593a2ff07650807432fa1
Mendes-Matheus <msmmendes211@gmail.com>
2025-10-24 18:22:38 -0300

    fix: corrige UsuarioResponseDTO

--- src/main/java/mendes/dev95/med_management_system_backend/domain/usuario/dto/UsuarioResponseDTO.java
+++ src/main/java/mendes/dev95/med_management_system_backend/domain/usuario/dto/UsuarioResponseDTO.java
@@ -1,11 +1,21 @@
 package mendes.dev95.med_management_system_backend.domain.usuario.dto;
 
+import mendes.dev95.med_management_system_backend.domain.usuario.entity.RolesUsuario;
+
 import java.util.UUID;
 
 
 public record UsuarioResponseDTO(
         UUID id,
+        String nome,
+        String cpf,
+        String logradouro,
+        String numero,
+        String cidade,
+        String telefone,
+        String email,
         String username,
-        String email
+        String password,
+        RolesUsuario role
 ) {}
 
